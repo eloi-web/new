1ec9e3bbd559bc24ebca963b4b9dfc40da6ae371
Miguel <144363628+eloi-web@users.noreply.github.com>
2025-06-02 19:43:03 +0000

    login

--- log-in.js
+++ log-in.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log("Admin login successful:", data.message);
                     alert("Admin login successful! Redirecting to admin dashboard.");
                     // You would redirect to your admin dashboard page here
-                    window.location.href = '/admin-dashboard.html'; // Create this page later
+                    window.location.href = '/dashboard.html'; // Create this page later
                 } else {
                     // Handle errors from your backend (e.g., 401 Invalid credentials)
                     console.error("Admin login failed:", data.message);
