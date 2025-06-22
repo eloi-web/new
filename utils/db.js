83d40aa9c8a65038c4ffe5fec2b36e54844c773a
Miguel <144363628+eloi-web@users.noreply.github.com>
2025-06-22 11:31:55 +0000

    cc

--- utils/db.js
+++ utils/db.js
@@ -9,10 +9,7 @@ async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
